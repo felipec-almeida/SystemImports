bdd74aedf2643936328a4f29a0933f102b98110d
Felipe <felipe@fullsoft.com.br>
2024-09-16 19:24:28 -0300

    Login

---
+++ Main.Application.SystemImports/Model/BaseAuthenticationResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Main.Application.SystemImports.Model
+{
+    public class BaseAuthenticationResponse<T>
+    {
+        [JsonPropertyName("response")]
+        public BaseCrudResponse<T> Response { get; set; }
+
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+
+        [JsonConstructor]
+        public BaseAuthenticationResponse(BaseCrudResponse<T> response, string token)
+        {
+            this.Response = response;
+            this.Token = token;
+        }
+    }
+}
---
+++ Main.Application.SystemImports/Model/BaseErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Main.Application.SystemImports.Model
+{
+    public class BaseErrorResponse
+    {
+        [JsonPropertyName("value")]
+        public string Value { get; set; }
+
+        [JsonPropertyName("status")]
+        public Main.Application.SystemImports.Model.HttpModel Status { get; set; }
+
+        public BaseErrorResponse(string value, Main.Application.SystemImports.Model.HttpModel httpResponse)
+        {
+            this.Value = value;
+            this.Status = httpResponse;
+        }
+    }
+}
